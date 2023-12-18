ae07937dedc634dcb5256bf07ff8c53404333028
SpStigma <yoann.rivet0@gmail.com>
2023-12-18 10:37:34 +0100

    First attempt of 0. Gather data from an API.

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Gather data from an API for a given employee
+ID and display TODO list progress."""
+import requests
+import sys
+
+
+def to_do(employee_ID):
+    """
+    Retrieve employee information and TODO
+    list progress based on the employee ID.
+
+    Args:
+        employee_ID (int): The ID of the employee.
+
+    Returns:
+        None
+
+    Prints:
+        Displays the employee's TODO list progress.
+    """
+    url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f"{url}/users/{employee_ID}"
+    todos_url = f"{url}/todos?userId={employee_ID}"
+
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    if employee_response.status_code == 200:
+        employee_name = employee_data.get('name')
+
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    if todos_response.status_code == 200:
+        total_tasks = len(todos_data)
+        completed_tasks = 0
+    for task in todos_data:
+        completed_tasks += task['completed']
+
+    incomplete_tasks = total_tasks - completed_tasks
+
+    print(f"Employee {employee_name} is done\
+          with tasks ({completed_tasks}/{total_tasks}):")
+
+    for task in todos_data:
+        if task['completed']:
+            print("\t{}".format(task['title']))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("error")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    to_do(employee_id)
